ef3fb3b92040ee6c8e87d7ce9483992eeceddac6
Alberto Kitumba <alberkitumba@gmail.com>
2025-07-19 20:01:27 +0100

    feat: change the font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,20 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import type React from "react";
-import "./globals.css";
+import { ThemeProvider } from '@/components/theme-provider';
+import type { Metadata } from 'next';
+import { Ubuntu } from 'next/font/google';
+import type React from 'react';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['300', '400', '500', '700'] });
 
 export const metadata: Metadata = {
-  title: "Pagador.ao - Sistema de RH e Folha de Pagamento",
-  description:
-    "Sistema completo de gestão de recursos humanos e folha de pagamento",
+  title: 'Pagador.ao - Sistema de RH e Folha de Pagamento',
+  description: 'Sistema completo de gestão de recursos humanos e folha de pagamento',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={ubuntu.className}>
         <ThemeProvider defaultTheme="light">
           <div>{children}</div>
         </ThemeProvider>
