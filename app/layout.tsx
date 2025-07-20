410a4f9eb7cb8fd5ffa2a5d9334ee7c2bdeb6a25
Alberto Kitumba <alberkitumba@gmail.com>
2025-07-20 17:34:18 +0100

    feat: change the document language

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
+    <html lang="pt" suppressHydrationWarning>
       <body className={ubuntu.className}>
         <ThemeProvider defaultTheme="light">
           <div>{children}</div>
