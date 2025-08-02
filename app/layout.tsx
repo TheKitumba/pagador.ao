a09f80226c81c7891f2260f8ae510c54ace3a91a
Alberto Kitumba <alberkitumba@gmail.com>
2025-08-02 11:24:16 +0100

    chore: change font family to `Nunito`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import type { Metadata } from 'next';
-import { Ubuntu } from 'next/font/google';
+import { Nunito } from 'next/font/google';
 import type React from 'react';
 import './globals.css';
 
-const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['300', '400', '500', '700'] });
+const nunito = Nunito({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export const metadata: Metadata = {
   title: 'Pagador.ao - Sistema de RH e Folha de Pagamento',
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt" suppressHydrationWarning>
-      <body className={ubuntu.className}>
+      <body className={nunito.className}>
         <ThemeProvider defaultTheme="light">
           <div>{children}</div>
         </ThemeProvider>
